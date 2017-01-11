5cf89af14c7ede32465015bce0edf2eec8d6ba70
huangbiyu <h417652303@163.com>
2017-01-11 20:30:28 +0800

    Update project structure

--- project/Builds.scala
+++ project/Builds.scala
@@ -14,11 +14,11 @@ object Builds extends Build {
     settings(assemblyJarName in assembly := s"${NamePrefix}SparkDemo_0.0.1.jar")
     .settings(mainClass in assembly := Some("com.wallace.spark.SparkDemo.DataFrameDemo.DataFrameDemo"))
 
-  // lazy val testConsumer = project.in(file("rdd/test")).settings(name := NamePrefix + "test").
-  //   settings(Common.settings: _*).
-  //   settings(libraryDependencies ++= Dependencies.sparkAppDependencies).
-  //   settings(assemblyOption in assembly := (assemblyOption in assembly).value.copy(includeScala = false)).
-  //   settings(assemblyJarName in assembly := s"${NamePrefix}test_0.0.1.jar")
+   lazy val scalaDemo = project.in(file("rdd/ScalaDemo")).settings(name := NamePrefix + "ScalaDemo").
+     settings(Common.settings: _*).
+     settings(libraryDependencies ++= Dependencies.commonDependencies).
+     settings(assemblyOption in assembly := (assemblyOption in assembly).value.copy(includeScala = false)).
+     settings(assemblyJarName in assembly := s"${NamePrefix}ScalaDemo_0.0.1.jar")
 
   // lazy val intelligentAnalysis = project.in(file("rdd/IntelligentAnalysis")).settings(name := NamePrefix + "IntelligentAnalysis").
   //   settings(Common.settings: _*).
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,16 +1,16 @@
 import sbt._
 
 object Dependencies {
-  val testDependencies: Seq[ModuleID] = Seq(
+  val commonDependencies: Seq[ModuleID] = Seq(
     "com.typesafe" % "config" % "1.2.1",
     "org.scalatest" % "scalatest_2.11" % "2.2.4",
     "mysql" % "mysql-connector-java" % "5.1.31" % "test",
-    "ch.ethz.ganymed" % "ganymed-ssh2" % "build210",
-    "org.apache.kafka" % "kafka-clients" % "0.8.2.1",
-    "org.apache.spark" % "spark-streaming-kafka-0-8_2.11" % "2.0.0"
+    "ch.ethz.ganymed" % "ganymed-ssh2" % "build210"
   )
 
   val sparkDependencies: Seq[ModuleID] = Seq(
+    "org.apache.kafka" % "kafka-clients" % "0.8.2.1",
+    "org.apache.spark" % "spark-streaming-kafka-0-8_2.11" % "2.0.0",
     "org.apache.spark" % "spark-core_2.11" % "2.0.0",
     "org.apache.spark" % "spark-sql_2.11" % "2.0.0",
     "org.apache.spark" % "spark-hive_2.11" % "2.0.0",
@@ -18,5 +18,5 @@ object Dependencies {
     "org.apache.spark" % "spark-mllib_2.11" % "2.0.0"
   )
 
-  val sparkAppDependencies: Seq[ModuleID] = testDependencies ++ sparkDependencies
+  val sparkAppDependencies: Seq[ModuleID] = commonDependencies ++ sparkDependencies
 }
