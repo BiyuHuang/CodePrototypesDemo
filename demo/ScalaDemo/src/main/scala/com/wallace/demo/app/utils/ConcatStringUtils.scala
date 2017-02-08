32d188eae4332826adf7bbac71bc71548a953bf6
huangbiyu <h417652303@163.com>
2017-02-08 19:31:03 +0800

    update ConcatStringUtils.scala

--- demo/ScalaDemo/src/main/scala/com/wallace/demo/app/utils/ConcatStringUtils.scala
+++ demo/ScalaDemo/src/main/scala/com/wallace/demo/app/utils/ConcatStringUtils.scala
@@ -9,40 +9,47 @@ import scala.collection.mutable.ArrayBuffer
   */
 object ConcatStringUtils extends LogSupport {
   def main(args: Array[String]) {
-    val str = "a1,b2,c3,d4,e5,3,n1,n11,n12,n2,n21,n22,n3,n31,n32,f6,g8,h9"
+    val str = "a1,b2,c3,d4,e5,4,n1,n11,n12,n13,n2,n21,n22,n23,n3,n31,n32,n33,n4,n41,n42,n43,f6,g8,h9"
     val temp = str.split(",", -1)
-    val result = concatNColumn(temp, 5, 3)
+    val result = concatNColumn(temp, 5, 4) // a1,b2,c3,d4,e5,4,n1$n2$n3$n4,n11$n21$n31$n41,n12$n22$n32$n42,n13$n23$n33$n43,f6,g8,h9
     for (elem <- result) {
       log.info("######## " + elem)
     }
+    log.info("@@@@@@ " + result.mkString(","))
   }
 
-  /** 支持连续多个可变长的字段 **/
+  /** 支持连续多个可变长的字段, 返回结果的字段个数是固定的. **/
   def concatNColumn(data: Array[String], index: Int, nLength: Int): Array[String] = {
     val nNum: Int = data(index).toInt
-    val tailData = data.slice(index + 1 + nNum * 3, data.length)
+    val tailData = data.slice(index + 1 + nNum * nLength, data.length)
     val tempResultData = new ArrayBuffer[String]()
     (0 to index).foreach {
       i =>
         tempResultData.append(data(i))
     }
 
-    val nData = data.drop(index + 1).slice(0, nNum * 3)
+    val nData = data.drop(index + 1).slice(0, nNum * nLength)
 
     val n1_Data = new ArrayBuffer[String]()
     val n2_Data = new ArrayBuffer[String]()
     val n3_Data = new ArrayBuffer[String]()
+    val n4_Data = new ArrayBuffer[String]()
+
     nData.indices.foreach {
       case i if i % nLength == 0 => n1_Data.append(nData(i))
       case i if i % nLength == 1 => n2_Data.append(nData(i))
       case i if i % nLength == 2 => n3_Data.append(nData(i))
+      case i if i % nLength == 3 => n4_Data.append(nData(i))
     }
     val n1_Res = n1_Data.result.mkString("$")
     val n2_Res = n2_Data.result.mkString("$")
     val n3_Res = n3_Data.result.mkString("$")
+    val n4_Res = n4_Data.result().mkString("$")
+
     tempResultData.append(n1_Res)
     tempResultData.append(n2_Res)
     tempResultData.append(n3_Res)
+    tempResultData.append(n4_Res)
 
     tempResultData.result().toArray ++ tailData
   }
