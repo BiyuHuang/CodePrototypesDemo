b5550c720557707e787025cde3df69d7849a4516
huangbiyu <h417652303@163.com>
2017-04-20 23:45:38 +0800

    #Wallace# UPDATE DataFrameDemo.scala

--- demo/SparkDemo/src/main/scala/com/wallace/spark/SparkDemo/DataFrameDemo/DataFrameDemo.scala
+++ demo/SparkDemo/src/main/scala/com/wallace/spark/SparkDemo/DataFrameDemo/DataFrameDemo.scala
@@ -1,9 +1,9 @@
 package com.wallace.spark.SparkDemo.DataFrameDemo
 
-import com.wallace.spark.SparkDemo.DataFrameDemo.PersonInfo._
-import com.wallace.spark.SparkDemo.DataFrameDemo.SpendingInfo._
 import com.wallace.common.LogSupport
 import com.wallace.common.TimeFormat.TimePara
+import com.wallace.spark.SparkDemo.DataFrameDemo.PersonInfo._
+import com.wallace.spark.SparkDemo.DataFrameDemo.SpendingInfo._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
@@ -20,8 +20,6 @@ case class Person(NAME: String, AGE: Int, GENDER: String, MARITAL_STATUS: String
 case class Customer(Time: String, Id: String, Spending: Int)
 
 object DataFrameDemo extends LogSupport {
-
-  def padto(ls: Array[String], columnNum: Int = 5): Array[String] = if (ls.length > columnNum) ls.dropRight(ls.length - columnNum) else ls.padTo(columnNum, "")
 
   def main(args: Array[String]) {
     val warehouseLocation = System.getProperty("user.dir").replaceAll("\\\\", "/") + "/" + "spark-warehouse"
@@ -40,7 +38,7 @@ object DataFrameDemo extends LogSupport {
     /**
       * Personal Information
       */
-    val data: Array[String] = Array("Wallace Huang,26,Male,Single,Basketball", "Bruce Wade,24,Male,Married,Book", "Lucy Adan,21,Female,Single,Computer", "Lina Anna,27,Female,Married,Shopping", "David Han,30,Male,Married,Reading")
+    val data: Array[String] = Array("Wallace Huang,26,Male,Single,Basketball", "Wallace Huang,26,Male,Single,Music", "Bruce Wade,24,Male,Married,Book", "Lucy Adan,21,Female,Single,Computer", "Lina Anna,27,Female,Married,Shopping", "David Han,30,Male,Married,Reading")
     val personData = sc.makeRDD(data, 2)
     //    personData.foreach(column => println(column))
     //    log.error("#########" + personData.collect.toList)
@@ -58,7 +56,7 @@ object DataFrameDemo extends LogSupport {
     /**
       * Purchase Something
       */
-    val purchase: Array[String] = Array(s"${TimePara.getCurrentDate},Wallace Huang,1900", "2016-06-01 08:01:00,Bruce Wade,",
+    val purchase: Array[String] = Array(s"${TimePara.getCurrentDate},Wallace Huang,1900", "2017-04-01 09:01:00,Wallace Huang,1900", "2016-06-01 08:01:00,Bruce Wade,",
       "2016-05-31 10:01:00,Lucy Adan,1056", "2016-05-23 09:00:00,Lina Anna,912", "2016-04-23 19:01:32,David Han,182")
     val purchaseRdd = sc.makeRDD(purchase, 2)
     val schema: StructType = StructType(Array(StructField("Time", StringType, nullable = true), StructField("Id", StringType, nullable = true), StructField("Spending", IntegerType, nullable = true)))
@@ -67,7 +65,7 @@ object DataFrameDemo extends LogSupport {
     //    val purchaseDF = purchaseRdd.map(line => padto(line.split(",", -1)))
     //      .map(column => Customer(column(Time.id), column(Id.id), column(Spending.id).toInt)).toDF
     purchaseDF.createOrReplaceTempView("spending_info")
-    val res2: DataFrame = spark.sql(s"Select Time,Id,Spending From spending_info Order by Time")
+    val res2: DataFrame = spark.sql(s"Select ROW_NUMBER() OVER(Partition by ID Order by Time Desc) as Row_ID,Time,Id,Spending From spending_info")
     res2.show(3)
 
     /**
@@ -83,5 +81,9 @@ object DataFrameDemo extends LogSupport {
     /** select */
     val res5 = res4.select("NAME", "Id", "Time", "Spending")
     res5.show(3)
+
+    //val temp= res1
   }
+
+  def padto(ls: Array[String], columnNum: Int = 5): Array[String] = if (ls.length > columnNum) ls.dropRight(ls.length - columnNum) else ls.padTo(columnNum, "")
 }
