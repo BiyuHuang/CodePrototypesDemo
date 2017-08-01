3b63205be173f1e356616f15173ea35286e19709
huangbiyu <h417652303@163.com>
2017-08-01 20:03:52 +0800

    update StrategyDemoRunTest.scala

--- demo/SparkDemo/src/main/scala/org/apache/spark/streaming/flume/SparkStreamingFlume.scala
+++ demo/SparkDemo/src/main/scala/org/apache/spark/streaming/flume/SparkStreamingFlume.scala
@@ -5,10 +5,16 @@ import org.apache.spark.SparkContext
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+import scala.util.control.NonFatal
+
 /**
   * Created by Wallace on 2017/3/30.
   */
 object SparkStreamingFlume extends LogSupport {
+  private val DEFAULT_BATCH_DURATION: Int = 20
+
+  private val DEFAULT_PART_NUM: Int = 30
+
   def main(args: Array[String]): Unit = {
 
     if (args.length < 2) {
@@ -20,7 +26,7 @@ object SparkStreamingFlume extends LogSupport {
     val sc = new SparkContext("local[*]", "Spark Streaming Flume Integration")
 
     //创建StreamingContext，20秒一个批次
-    val ssc = new StreamingContext(sc, Seconds(20))
+    val ssc = new StreamingContext(sc, Seconds(DEFAULT_BATCH_DURATION))
 
     val hostname = args(0)
     val port = args(1).toInt
@@ -32,7 +38,7 @@ object SparkStreamingFlume extends LogSupport {
 
     flumeStream.foreachRDD {
       rdd =>
-        rdd.coalesce(30).saveAsTextFile("/")
+        rdd.coalesce(DEFAULT_PART_NUM).saveAsTextFile("/")
     }
     //开始运行
     ssc.start()
@@ -40,7 +46,8 @@ object SparkStreamingFlume extends LogSupport {
     try {
       ssc.awaitTermination()
     } catch {
-      case _: Exception =>
+      case NonFatal(e) =>
+        log.error(s"[SparkStreamingFlume] Catch NonFatal Exception: ${e.getMessage}.")
         ssc.stop(stopSparkContext = true, stopGracefully = true)
     } finally {
       sc.stop()
