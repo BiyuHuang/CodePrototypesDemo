0ae1f167b1a37872ff6930d6e171f149cfc0d60a
huangbiyu <h417652303@163.com>
2017-11-07 23:07:43 +0800

    update ForecastInOrOutDoorMR demo

--- demo/SparkDemo/src/main/scala/com/wallace/spark/sparkmllibdemo/ForecastIndoorAndOutdoorMR.scala
+++ demo/SparkDemo/src/main/scala/com/wallace/spark/sparkmllibdemo/ForecastIndoorAndOutdoorMR.scala
@@ -9,6 +9,7 @@
 package com.wallace.spark.sparkmllibdemo
 
 import com.wallace.spark.CreateSparkSession
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
@@ -21,7 +22,9 @@ object ForecastIndoorAndOutdoorMR extends CreateSparkSession {
     val rdd = spark.sparkContext.textFile("./demo/SparkDemo/src/main/resources/trainingData.csv")
     import spark.implicits._
 
-    val srcDF: DataFrame = rdd.map(x => x.split(",")).map(e =>
+
+    val splitRdds: Array[RDD[String]] = rdd.randomSplit(Array(0.7, 0.3), seed = 11L) //Split data into training (70%) and test(30%)
+    val srcDF: DataFrame = splitRdds.head.map(x => x.split(",")).map(e =>
       DataFields(e(0), e(1).trim.toInt, e(2).trim.toInt, e(3).trim.toDouble, e(4).trim.toDouble, e(5).trim.toDouble, e(6).trim.toInt,
         e(7).trim.toInt, e(8), e(9), e(10), e(11), e(12), e(13), e(14).trim.toInt)).toDF()
     srcDF.show(1)
