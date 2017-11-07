b2a406525b02b0e803655cc09848c37c6114634d
huangbiyu <h417652303@163.com>
2017-11-07 23:59:06 +0800

    update ForecastInOrOutDoorMR demo

--- demo/SparkDemo/src/main/scala/com/wallace/spark/sparkmllibdemo/ForecastIndoorAndOutdoorMR.scala
+++ demo/SparkDemo/src/main/scala/com/wallace/spark/sparkmllibdemo/ForecastIndoorAndOutdoorMR.scala
@@ -9,6 +9,10 @@
 package com.wallace.spark.sparkmllibdemo
 
 import com.wallace.spark.CreateSparkSession
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
@@ -21,17 +25,45 @@ object ForecastIndoorAndOutdoorMR extends CreateSparkSession {
     spark.sql("use default")
     val rdd = spark.sparkContext.textFile("./demo/SparkDemo/src/main/resources/trainingData.csv")
 
-
     val splitRdds: Array[RDD[String]] = rdd.randomSplit(Array(0.7, 0.3), seed = 5L) //Split data into training (70%) and test(30%)
-    log.info(splitRdds.length)
-    val trainData = splitRdds.head.map(x => x.split(","))
-    //.map(e =>
-    //DataFields(e(0), e(1).trim.toInt, e(2).trim.toInt, e(3).trim.toDouble, e(4).trim.toDouble, e(5).trim.toDouble, e(6).trim.toInt,
-    //  e(7).trim.toInt, e(8), e(9), e(10), e(11), e(12), e(13), e(14).trim.toInt)).toDF()
+    val trainingData: RDD[LabeledPoint] = splitRdds.head.map(x => x.split(",")).map(x => LabeledPoint(
+      x.last.toInt,
+      Vectors.dense(Array(x(2).toDouble, x(3).toDouble,
+        x(4).toDouble, x(5).toDouble, x(6).toDouble, x(7).toDouble))))
 
-    log.info(s"Training Data Size: ${trainData.count()}.")
-    val testData = splitRdds.last
+    log.info(s"Training Data Size: ${trainingData.count()}.")
+    val testData: RDD[LabeledPoint] = splitRdds.last.map(x => x.split(",")).map(x => LabeledPoint(
+      x.last.toInt,
+      Vectors.dense(Array(x(2).toDouble, x(3).toDouble,
+        x(4).toDouble, x(5).toDouble, x(6).toDouble, x(7).toDouble))))
+
     log.info(s"Test Data Size : ${testData.count()}.")
+
+    // Run training algorithm to build the model
+    val numIterations = 100
+    val model = SVMWithSGD.train(trainingData, numIterations)
+    // Clear the default threshold
+    model.clearThreshold()
+    //Compute raw scores on the testData
+    val scoreAndLabels: RDD[(Double, Double)] = testData.map {
+      point =>
+        val score = model.predict(point.features)
+        (score, point.label)
+    }
+    log.info(s"Compute raw scores on the testData")
+    scoreAndLabels.foreach(x => log.info(s"score: ${x._1},label: ${x._2}."))
+    /**
+      * Get evaluation metrics.得到评估指标
+      * 以召回率为y轴，以特异性为x轴，我们就直接得到了RoC曲线
+      * 召回率越高，特异性越小，RoC曲线越靠近左上角，模型和算法就越高效
+      * 另一方面，如果ROC是光滑的，那么基本可以判断没有太大的过拟合（overfitting)
+      * 从几何的角度讲，RoC曲线下方的面积越大越大，则模型越优。
+      * 所以有时候我们用RoC曲线下的面积，即AUC（Area Under Curve）值来作为算法和模型好坏的标准
+      **/
+    val metrics = new BinaryClassificationMetrics(scoreAndLabels)
+    val auROC = metrics.areaUnderROC()
+
+    log.info("Area under ROC = " + auROC)
 
   }
 
