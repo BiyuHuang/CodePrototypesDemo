8845e9ad0daf0561457c1deaf530173bc9dded28
WallaceHuang <h417652303@163.com>
2018-01-02 19:10:23 +0800

    add readFileByByteBuffer

--- demo/ScalaDemo/src/main/scala/com/wallace/demo/app/common/Using.scala
+++ demo/ScalaDemo/src/main/scala/com/wallace/demo/app/common/Using.scala
@@ -5,20 +5,20 @@ import com.wallace.demo.app.utils.FuncRuntimeDur
 import scala.util.control.NonFatal
 
 trait Using extends FuncRuntimeDur {
-  protected def using[A <: {def close() : Unit}, B](param: A)(f: A => B): B = {
-    try {
-      f(param)
-    } finally {
-      param.close()
-    }
-  }
-
   protected def usingWithErrMsg[A <: {def close() : Unit}, B](param: A, errMsg: String)(f: A => B): Unit = {
     try {
       f(param)
     } catch {
       case NonFatal(e) =>
         log.error(s"$errMsg: ", e)
+    } finally {
+      param.close()
+    }
+  }
+
+  protected def using[A <: {def close() : Unit}, B](param: A)(f: A => B): B = {
+    try {
+      f(param)
     } finally {
       param.close()
     }
