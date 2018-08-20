cf50a071f434c7632b2d59985bf78d78edcfa67e
WallaceHuang <h417652303@163.com>
2018-08-20 18:31:13 +0800

    20180820 WallaceHuang: update JsonFormatter.scala

--- demo/ScalaDemo/src/main/scala/com/wallace/demo/app/utils/JsonFormatter.scala
+++ demo/ScalaDemo/src/main/scala/com/wallace/demo/app/utils/JsonFormatter.scala
@@ -1,9 +1,10 @@
 package com.wallace.demo.app.utils
 
-import net.minidev.json.JSONObject
 import net.minidev.json.parser.JSONParser
+import net.minidev.json.{JSONArray, JSONObject}
 
 import scala.collection.JavaConverters._
+import scala.collection.Map
 
 /**
   * Created by 10192057 on 2018/7/30 0030.
@@ -11,12 +12,16 @@ import scala.collection.JavaConverters._
 object JsonFormatter {
   def format(jsonStr: String): Map[String, String] = {
     //import scala.util.parsing.json.JSON
-    val jParser: JSONParser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE)
-    val jObj: JSONObject = jParser.parse(jsonStr).asInstanceOf[JSONObject]
-    convertJsonToMap(jObj)
+    convertJsonToMap(convertStrToJson(jsonStr))
   }
 
-  private def convertJsonToMap(jsonObj: JSONObject, rootKey: String = ""): Map[String, String] = {
+  def convertStrToJson(jsonStr: String,
+                       permissifMode: Int = JSONParser.DEFAULT_PERMISSIVE_MODE): JSONObject = {
+    val jParser: JSONParser = new JSONParser(permissifMode)
+    jParser.parse(jsonStr).asInstanceOf[JSONObject]
+  }
+
+  def convertJsonToMap(jsonObj: JSONObject, rootKey: String = ""): Map[String, String] = {
     jsonObj.keySet().asScala.flatMap {
       key =>
         val tempVal: AnyRef = jsonObj.get(key)
@@ -24,10 +29,36 @@ object JsonFormatter {
         tempVal match {
           case nObject: JSONObject =>
             if (nObject.isEmpty) Map(fieldKey -> "") else convertJsonToMap(nObject, fieldKey)
+          case nArray: JSONArray =>
+            nArray.asScala.flatMap {
+              elem =>
+                convertJsonToMap(elem.asInstanceOf[JSONObject], fieldKey)
+            }
           case _ =>
             val value: String = if (tempVal == null) "" else tempVal.toString
             Map(fieldKey -> value)
         }
     }.toMap
+  }
+
+  def convertJsonToArray(jsonObj: JSONObject, rootKey: String = ""): Array[(String, String)] = {
+    jsonObj.keySet().asScala.flatMap {
+      key =>
+        val tempVal: AnyRef = jsonObj.get(key)
+        val fieldKey: String = if (rootKey.nonEmpty) rootKey + "." + key else key
+        tempVal match {
+          case nObject: JSONObject =>
+            if (nObject.isEmpty) Map(fieldKey -> "") else convertJsonToArray(nObject, fieldKey)
+          case nArray: JSONArray =>
+            nArray.asScala.flatMap {
+              elem =>
+                val temp = elem.asInstanceOf[JSONObject]
+                convertJsonToArray(temp, fieldKey)
+            }
+          case _ =>
+            val value: String = if (tempVal == null) "" else tempVal.toString
+            Array(fieldKey -> value)
+        }
+    }.toArray
   }
 }
