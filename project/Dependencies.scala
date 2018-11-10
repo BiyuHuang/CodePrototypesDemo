8b5030b5b16b9fb5582e10e22bbdf6119cba39b6
huangbiyu <h417652303@163.com>
2018-11-11 00:29:26 +0800

    20181108 WallaceHuang: update travis.yml

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 import sbt._
 
 object Dependencies {
@@ -9,8 +17,8 @@ object Dependencies {
     "org.slf4j" % "slf4j-api" % "1.7.7",
     "ch.qos.logback" % "logback-core" % "1.1.2",
     "ch.qos.logback" % "logback-classic" % "1.1.2",
-    "com.typesafe.akka" %% "akka-actor" % "2.5.3",
-    "com.typesafe.akka" %% "akka-remote" % "2.5.3",
+    "com.typesafe.akka" %% "akka-actor" % "2.5.16",
+    "com.typesafe.akka" %% "akka-remote" % "2.5.16",
     "com.github.pathikrit" %% "better-files" % "3.4.0",
     "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.0",
     "com.yammer.metrics" % "metrics-core" % "2.2.0",
@@ -33,13 +41,13 @@ object Dependencies {
 
   val sparkDependencies: Seq[ModuleID] = Seq(
     "org.apache.kafka" % "kafka-clients" % "0.10.0.1",
-    "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.0.2",
+    "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.2.0",
     "org.apache.spark" % "spark-streaming-flume_2.11" % "2.0.0",
-    "org.apache.spark" % "spark-core_2.11" % "2.0.2",
-    "org.apache.spark" % "spark-sql_2.11" % "2.0.2",
-    "org.apache.spark" % "spark-hive_2.11" % "2.0.2",
-    "org.apache.spark" % "spark-streaming_2.11" % "2.0.2",
-    "org.apache.spark" % "spark-mllib_2.11" % "2.0.2"
+    "org.apache.spark" % "spark-core_2.11" % "2.2.0",
+    "org.apache.spark" % "spark-sql_2.11" % "2.2.0",
+    "org.apache.spark" % "spark-hive_2.11" % "2.2.0",
+    "org.apache.spark" % "spark-streaming_2.11" % "2.2.0",
+    "org.apache.spark" % "spark-mllib_2.11" % "2.2.0"
   )
 
   val sparkAppDependencies: Seq[ModuleID] = commonDependencies ++ hdfsDependencies ++ sparkDependencies
