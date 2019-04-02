231c4a87ffd96ee5806d6c69dd67e58730f9eb59
wallace <h417652303@163.com>
2019-04-02 14:06:43 +0800

    [ADD] 20190402: add GccDemo

---
+++ demo/GccDemo/GccDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GccDemo
+//
+//  Created by 黄碧宇 on 2019/4/2.
+//  Copyright © 2019 黄碧宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
