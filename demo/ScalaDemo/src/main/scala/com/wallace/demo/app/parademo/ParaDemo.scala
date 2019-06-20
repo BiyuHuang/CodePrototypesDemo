a83de20ed750ccaa1bc92caba9db43bb74446043
wallace <h417652303@163.com>
2019-06-20 15:41:49 +0800

    [UPDATE] Update ParaDemo.scala

--- demo/ScalaDemo/src/main/scala/com/wallace/demo/app/parademo/ParaDemo.scala
+++ demo/ScalaDemo/src/main/scala/com/wallace/demo/app/parademo/ParaDemo.scala
@@ -15,20 +15,32 @@ import scala.concurrent.forkjoin.ForkJoinPool
   * Created by wallace on 2019/6/20.
   */
 object ParaDemo {
+  val testData: Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
   def main(args: Array[String]): Unit = {
     //TODO processing one by one
-    Array(1, 2, 3, 4, 5, 6, 7).map {
+    println("#### processing one by one")
+    testData.map {
       x =>
         println(s"[Thread: ${Thread.currentThread().getName}] Start to process $x @${System.currentTimeMillis()}")
         Thread.sleep(1000L)
         x + 1
     }
-
+    println(s"[Thread: ${Thread.currentThread().getName}] Succeed to process @${System.currentTimeMillis()}")
+    //TODO processing par
+    println(s"#### processing par @${scala.collection.parallel.ForkJoinTasks.defaultForkJoinPool.getParallelism}")
+    testData.par.map {
+      x =>
+        println(s"[Thread: ${Thread.currentThread().getName}] Start to process $x @${System.currentTimeMillis()}")
+        Thread.sleep(1000L)
+        x + 1
+    }
+    println(s"[Thread: ${Thread.currentThread().getName}] Succeed to process @${System.currentTimeMillis()}")
     //TODO concurrent processing
+    println(s"#### concurrent processing @3")
     import scala.collection.parallel._
-    val pc: ParArray[Int] = mutable.ParArray(1, 2, 3, 5, 6, 7)
-    val forkJoinPool: ForkJoinPool = new ForkJoinPool(7)
+    val pc: ParArray[Int] = mutable.ParArray(testData: _*)
+    val forkJoinPool: ForkJoinPool = new ForkJoinPool(3)
     pc.tasksupport = new ForkJoinTaskSupport(forkJoinPool)
     pc.map {
       x =>
@@ -37,7 +49,5 @@ object ParaDemo {
         x + 1
     }
     println(s"[Thread: ${Thread.currentThread().getName}] Succeed to process @${System.currentTimeMillis()}")
-
-
   }
 }
