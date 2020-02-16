fefdab2023768d40ac65a7800b03b4a85ecdce58
wallace <h417652303@163.com>
2020-02-16 21:18:17 +0800

    [UPDATE] Update pom.xml

--- demo/GccDemo/cdemo/main.c
+++ demo/GccDemo/cdemo/main.c
@@ -10,28 +10,36 @@
 #ifndef OS_TYPE
 #define OS_TYPE "macos"
 #endif
+
 long getsizeof()
 {
-    static char *kwlist[] = {"object", "default", 0};
-    long size;
-    
-    return 1L;
+    static char *kwlist[] = {"AAA", "BBB", 0};
+    auto long size;
+    size = sizeof(kwlist);
+    return size;
 }
-void print(int *s)
+void print(long *s)
 {
     printf("=> ");
-    printf("%d",*s);
+    printf("%ld",*s);
     printf("\n");
 }
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("=> C: Hello, World!\n");
     long s = getsizeof();
-    printf("=> getsizeof: %ld\n",s);
-    long *ts = &s;
-    print(ts);
-    char *p;
-    p = &OS_TYPE;
-    printf("=> %s\n", p);
+    printf("=> getsizeof: %ld Bytes\n",s);
+    printf("=> OS Name: %s\n", OS_TYPE);
+    
+    int var1;
+    char var2[10];
+    printf("=> address of var1: %p\n", &var1);
+    printf("=> address of var2: %p\n", &var2);
+    
+    /*
+     指针是一个变量，其值为另一个变量的地址，即内存位置的直接地址。
+     type *var-name;
+     */
+    char *ch;
     return 0;
 }
