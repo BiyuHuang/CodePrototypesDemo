557d11c9864986638eaf18946c9bca3291191585
80286616 <huangbiyu@oppo.com>
2021-09-01 11:36:34 +0800

    [UPDATE] Update .gitignore

--- demo/SparkDemo/src/main/scala/com/wallace/spark/sparkdemo/dataframedemo/DataFrameDemo.scala
+++ demo/SparkDemo/src/main/scala/com/wallace/spark/sparkdemo/dataframedemo/DataFrameDemo.scala
@@ -8,6 +8,8 @@
 
 package com.wallace.spark.sparkdemo.dataframedemo
 
+import java.io.{File, PrintWriter}
+
 import com.wallace.common.CreateSparkSession
 import com.wallace.common.timeformat.TimePara
 import com.wallace.spark.sparkdemo.dataframedemo.PersonInfo._
@@ -15,15 +17,17 @@ import com.wallace.spark.sparkdemo.dataframedemo.SpendingInfo.{Id, Spending, Tim
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.storage.StorageLevel
 
+import scala.io.{BufferedSource, Source}
 import scala.util.Try
 
 /**
-  * Created by Wallace on 2016/6/1.
-  * DataFrame Demo
-  */
+ * Created by Wallace on 2016/6/1.
+ * DataFrame Demo
+ */
 
 case class Person(NAME: String, AGE: Int, GENDER: String, MARITAL_STATUS: String, HOBBY: String)
 
@@ -55,11 +59,58 @@ object DataFrameDemo extends CreateSparkSession {
     StructField("col5", StringType, nullable = true),
     StructField("index", IntegerType, nullable = true)))
 
+  val spark: SparkSession = createSparkSession("DataFrameDemo")
+
+  import spark.implicits._
+
   def main(args: Array[String]): Unit = {
-    val spark: SparkSession = createSparkSession("DataFrameDemo")
     spark.conf.set("spark.driver.memory", "3g")
     sqlDemo(spark)
-    joinDemo(spark)
+    //    joinDemo(spark)
+    //    mysqlJdbcDemo(spark)
+  }
+
+  private def mysqlJdbcDemo(spark: SparkSession): Unit = {
+    val jdbc_url = s"jdbc:mysql://127.0.0.1:3306/demo?useUnicode=true&characterEncoding=utf-8&useSSL=false"
+    val mysqlTabDF: DataFrame = spark.read.format("jdbc")
+      .options(
+        Map("url" -> jdbc_url,
+          "driver" -> "com.mysql.jdbc.Driver",
+          "dbtable" -> "test",
+          "user" -> "root",
+          "password" -> "123456"))
+      .load()
+
+    mysqlTabDF.selectExpr("*").show(10)
+  }
+
+  def genRecallSeq(in: Int): Seq[String] = {
+    in.toBinaryString.reverse.zipWithIndex.filter(_._1 == '1').map(x => s"1<<${x._2}")
+  }
+
+  def readTabMetaData(inputFile: String, outPutFile: String): Unit = {
+    val w: PrintWriter = new PrintWriter(new File(outPutFile))
+    val s: BufferedSource = Source.fromFile(new File(inputFile))
+    val res = s.getLines().toArray
+    res.foreach {
+      tab =>
+        val metaData: Array[Row] = spark.sql(s"desc formatted ${tab}").collect()
+        val location: String = metaData
+          .filter(_.getAs[String](0).trim.toLowerCase == "location")
+          .map(x => x.getAs[String](1))
+          .head.trim
+        val partition = metaData
+          .filter(_.getAs[String](0) != "")
+          .map(x => s"${x.getAs[String](0).trim}@${x.getAs[String](1).trim}")
+          .mkString("\t")
+          .split("# Partition Information@\t# col_name@data_type\t")(1)
+          .split("\t# Detailed Table Information")(0)
+          .replaceAll("\t", "|")
+        val res: String = s"$tab,$location,$partition"
+        w.println(res)
+    }
+    s.close()
+    w.close()
   }
 
   private def joinDemo(spark: SparkSession): Unit = {
@@ -97,7 +148,6 @@ object DataFrameDemo extends CreateSparkSession {
 
   protected def udfFuncDemo(spark: SparkSession): Unit = {
     val sc: SparkContext = spark.sparkContext
-    import spark.implicits._
     val rdd: RDD[String] = sc.textFile("./demo/SparkDemo/src/main/resources/sample_1.csv")
     val srcDF: DataFrame = rdd.map(_.split(",", -1)).map {
       col =>
@@ -115,10 +165,9 @@ object DataFrameDemo extends CreateSparkSession {
 
   protected def sqlDemo(spark: SparkSession): Unit = {
     val sc: SparkContext = spark.sparkContext
-    import spark.implicits._
     /**
-      * Personal Information
-      */
+     * Personal Information
+     */
     val data: Array[String] = Array("Wallace Huang,26,Male,Single,Basketball", "Wallace Huang,26,Male,Single,Music", "Bruce Wade,24,Male,Married,Book", "Lucy Adan,21,Female,Single,Computer", "Lina Anna,27,Female,Married,Shopping", "David Han,30,Male,Married,Reading")
     val personData: RDD[String] = sc.makeRDD(data, 2)
     val personDS: Dataset[Person] = personData.map(line => padto(line.split(",", -1)))
@@ -127,14 +176,15 @@ object DataFrameDemo extends CreateSparkSession {
         column(GENDER.id),
         column(MARITAL_STATUS.id),
         column(HOBBY.id))).toDS()
-
+    personDS.write.mode(SaveMode.Append).parquet("/")
     personDS.createOrReplaceTempView("person_info")
     val res1: DataFrame = spark.sql(s"SELECT * FROM person_info")
+    res1.groupBy("col1").agg(collect_list(map($"11", $"aa")) as "col2")
     res1.map(x => x.getInt(1)).rdd.take(1)
     res1.show(3)
     /**
-      * Purchase Something
-      */
+     * Purchase Something
+     */
     val purchase: Array[String] = Array(s"${TimePara.getCurrentDate},Wallace Huang,1900", "2017-04-01 09:01:00,Wallace Huang,1900", "2016-06-01 08:01:00,Bruce Wade,",
       "2016-05-31 10:01:00,Lucy Adan,1056", "2016-05-23 09:00:00,Lina Anna,912", "2016-04-23 19:01:32,David Han,182")
     val purchaseRdd = sc.makeRDD(purchase, 2)
@@ -145,8 +195,8 @@ object DataFrameDemo extends CreateSparkSession {
     val res2: DataFrame = spark.sql(s"Select ROW_NUMBER() OVER(Partition by ID Order by Time Desc) as Row_ID,Time,Id,Spending From spending_info")
     res2.show(3)
     /**
-      * 两表联合查询
-      */
+     * 两表联合查询
+     */
     val res3: DataFrame = res1.join(res2, res1.col("NAME") === res2.col("Id")).orderBy("Time").where("Spending <> 0 ")
     res3.show(3)
     val res4: DataFrame = res3.filter(res3.col("Time") >= "2016-05-23 09:00:00")
@@ -157,7 +207,7 @@ object DataFrameDemo extends CreateSparkSession {
     //    res5.write.format("com.databricks.spark.csv").mode(SaveMode.Overwrite).save("./temp/")
     //    res5.write.format("csv").mode(SaveMode.Overwrite).save("/")
     //res5.write.format("csv").mode(SaveMode.Overwrite).save("/")
-    //res5.write.mode(SaveMode.Append).parquet("/")
+    //    res5.write.mode(SaveMode.Append).parquet("/")
   }
 
   protected def padto(ls: Array[String], columnNum: Int = 5): Array[String] = if (ls.length > columnNum) ls.dropRight(ls.length - columnNum) else ls.padTo(columnNum, "")
