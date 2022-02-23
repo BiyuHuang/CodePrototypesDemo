eae220b08303d3f8cd39652a37d3668dc31dc9c9
huangbiyu <huangbiyu@oppo.com>
2022-02-23 15:24:15 +0800

    update
    update code

--- demo/ScalaDemo/src/main/scala/com/wallace/demo/app/algorithmdemo/BitMap.scala
+++ demo/ScalaDemo/src/main/scala/com/wallace/demo/app/algorithmdemo/BitMap.scala
@@ -1,35 +1,28 @@
 package com.wallace.demo.app.algorithmdemo
 
 /**
-  * Created by wallace on 2019/10/28.
-  */
+ * Created by wallace on 2019/10/28.
+ */
 class BitMap(size: Int) extends Cloneable with Serializable {
   private final val SIZE: Int = size
   private val BM: Array[Int] = new Array[Int](SIZE / 32 + 1)
 
-  def insert(a: Int): Unit = {
+  def insert(e: Int): Unit = {
     // e / 32 为十进制在数组BM中的下标
-    val index = a >> 5
+    val index: Int = e >> 5
     // e % 32 为十进制在数据BM(index)中的下标
-    BM(index) |= 1 << (a & 0x1F)
+    BM(index) |= 1 << (e & 0x1F)
   }
 
-  def exists(a: Int): Boolean = {
-    val index = a >> 5
-    ((BM(index) >> (a & 0x1F)) & 1) == 1
-  }
+  def exists(a: Int): Boolean = getValue(a) == 1
 
-  def getValue(a: Int): Int = {
-    val tmp: Int = BM(a >> 5) >> (a & 0x1F)
-    tmp & 1
-  }
-
+  def getValue(e: Int): Int = BM(e >> 5) >> (e & 0x1F) & 1
 
   def show(num: Int): Unit = {
     (0 until num).foreach {
       i =>
         val tmpVal: Array[Int] = new Array[Int](32)
-        var temp = BM(i)
+        var temp: Int = BM(i)
         tmpVal.indices.foreach {
           j =>
             tmpVal(j) |= (temp & 1)
@@ -42,8 +35,8 @@ class BitMap(size: Int) extends Cloneable with Serializable {
 
 object BitMap {
   def main(args: Array[String]): Unit = {
-    val bMap = new BitMap(320)
-    //    Array(1, 2, 3, 5, 8, 30, 32, 64, 56, 159, 120, 21, 17, 35, 45, 320).foreach(bMap.insert)
+    val bMap: BitMap = new BitMap(320)
+    Array(1, 2, 3, 5, 8, 30, 32, 64, 56, 159, 120, 21, 17, 35, 45, 320).foreach(bMap.insert)
     Array(2, 3, 5, 8).foreach(bMap.insert)
 
     println(s">>> Key: 159, Value: ${bMap.getValue(159)}.")
