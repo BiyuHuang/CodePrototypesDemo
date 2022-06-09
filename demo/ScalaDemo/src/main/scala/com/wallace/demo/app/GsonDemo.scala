ff7e57365213fb893deb5433525ec5dbc3693ec5
Wallace Huang <h417652303@163.com>
2022-06-09 14:28:10 +0800

    merge Dev into master (#68)
    * Update README.md
    
    * Update README.md
    
    * Create codacy.yml
    
    * Delete codacy.yml
    
    * Bump json-smart from 2.4.1 to 2.4.4
    
    Bumps [json-smart](https://github.com/netplex/json-smart-v2) from 2.4.1 to 2.4.4.
    - [Release notes](https://github.com/netplex/json-smart-v2/releases)
    - [Commits](https://github.com/netplex/json-smart-v2/commits)
    
    ---
    updated-dependencies:
    - dependency-name: net.minidev:json-smart
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * add GsonDemo.scala
    
    * add GsonDemo: update json data
    
    * update pom.xml
    
    * update pom.xml
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ demo/ScalaDemo/src/main/scala/com/wallace/demo/app/GsonDemo.scala
@@ -0,0 +1,95 @@
+package com.wallace.demo.app
+
+import com.google.gson._
+import com.wallace.demo.app.common.LogSupport
+
+import java.lang.{Integer => JInt, Long => JLong}
+import java.math.BigDecimal
+
+/**
+ * Author: wallace
+ * Date: 2022/5/22 09:20
+ * Description: Gson Demo
+ */
+object GsonDemo extends LogSupport {
+  private val jsonStr: String = """{"key1":[{"key1_1":1000,"key1_2":11111111111111111},{"key1_1":2000,"key1_2":-123457}],"key2":[1,2,3,4,5,6,-1],"key3":-30,"key4":{"key4_1":-2,"key4_2":"test"}}"""
+  private val jsonParser: JsonParser = new JsonParser()
+
+  def main(args: Array[String]): Unit = {
+    val jsonObj: JsonElement = jsonParser.parse(jsonStr)
+    log.info(jsonObj.getAsJsonObject.toString)
+
+    val needFixNode: Map[String, String] = Map("key4.key4_1" -> "UINT32",
+      "key1.key1_2" -> "UINT64",
+      "key2" -> "UINT32",
+      "key3" -> "UINT64",
+      "not-existed-key" -> "UINT64")
+    adjustJsonNode(jsonObj, needFixNode)
+
+    log.info(jsonObj.getAsJsonObject.toString)
+  }
+
+  def adjustJsonNode(jsonElement: JsonElement, jsonNodes: Map[String, String]): Unit = {
+    if (jsonNodes.nonEmpty) {
+      jsonNodes.foreach {
+        case (jsonNodePath, dataType) =>
+          jsonNodePath.split("\\.").toList match {
+            case parent :: Nil =>
+              jsonElement match {
+                case _: JsonPrimitive | _: JsonArray | _: JsonNull => // all of these cases should be ignored.
+                case jsonObject: JsonObject =>
+                  if (jsonObject.has(parent)) {
+                    jsonObject.get(parent) match {
+                      case _: JsonObject | _: JsonNull => // all of these cases should be ignored.
+                      case jp: JsonPrimitive =>
+                        val newValue: JsonPrimitive = correctNumeric(jp.getAsNumber, dataType)
+                        // jsonObject.remove(parent)
+                        jsonObject.add(parent, newValue)
+                      case jsonArray: JsonArray =>
+                        // only support Array[JsonPrimitive], can't be used for Array[JsonObject]
+                        val newJsonArray: JsonArray = new JsonArray()
+                        (0 until jsonArray.size()).foreach {
+                          i: Int =>
+                            val newValue: JsonPrimitive = correctNumeric(jsonArray.get(i).getAsNumber, dataType)
+                            newJsonArray.add(newValue)
+                        }
+                        // jsonObject.remove(parent)
+                        jsonObject.add(parent, newJsonArray)
+                    }
+                  }
+              }
+            case parent :: children =>
+              val childrenNode: Map[String, String] = Map(children.mkString(".") -> dataType)
+              jsonElement match {
+                case _: JsonPrimitive | _: JsonNull | _: JsonArray => // all of these cases should be ignored.
+                case jsonObject: JsonObject =>
+                  if (jsonObject.has(parent)) {
+                    jsonObject.get(parent) match {
+                      case jsonArray: JsonArray =>
+                        val newJsonArray = new JsonArray()
+                        (0 until jsonArray.size()).foreach {
+                          i: Int =>
+                            adjustJsonNode(jsonArray.get(i), childrenNode)
+                            newJsonArray.add(jsonArray.get(i))
+                        }
+                      case _: JsonObject =>
+                        adjustJsonNode(jsonObject.get(parent), childrenNode)
+                    }
+                  }
+              }
+          }
+      }
+    }
+  }
+
+  def correctNumeric[T <: Number](data: T, dataType: String): JsonPrimitive = {
+    dataType match {
+      case "UINT32" =>
+        new JsonPrimitive(JInt.toUnsignedLong(JInt.valueOf(data.intValue())))
+      case "UINT64" =>
+        new JsonPrimitive(new BigDecimal(JLong.toUnsignedString(JLong.valueOf(data.longValue()))))
+      case _ =>
+        throw new UnsupportedOperationException(s"unsupported operation for dataType: $dataType.")
+    }
+  }
+}
