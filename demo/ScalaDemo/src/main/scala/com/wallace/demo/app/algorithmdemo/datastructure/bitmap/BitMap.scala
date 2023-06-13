b2324559b743aa28546f5abb0016c49ea4a421cf
Wallace Huang <h417652303@gmail.com>
2023-06-13 17:39:00 +0800

    feat(scala-demo): add bit filter

--- demo/ScalaDemo/src/main/scala/com/wallace/demo/app/algorithmdemo/datastructure/bitmap/BitMap.scala
+++ demo/ScalaDemo/src/main/scala/com/wallace/demo/app/algorithmdemo/datastructure/bitmap/BitMap.scala
@@ -3,26 +3,28 @@ package com.wallace.demo.app.algorithmdemo.datastructure.bitmap
 /**
  * Created by wallace on 2019/10/28.
  */
-class BitMap(size: Int) extends Cloneable with Serializable {
-  private final val SIZE: Int = size
-  private val BM: Array[Int] = new Array[Int](SIZE / 32 + 1)
+class BitMap(size: Long) extends Cloneable with Serializable {
+  private final val SIZE: Int = (size / 64 + 1).toInt
+  assert(SIZE > 0, s"too large size: $size")
+  private val BM: Array[Long] = new Array[Long](SIZE)
 
-  def insert(e: Int): Unit = {
+  def insert(e: Long): Unit = {
     // e / 32 为十进制在数组BM中的下标
-    val index: Int = e >> 5
+    val index: Int = (e >> 6).toInt
+    assert(index >= 0, s"$e is greater than $SIZE")
     // e % 32 为十进制在数据BM(index)中的下标
-    BM(index) |= 1 << (e & 0x1F)
+    BM(index) |= 1L << (e & 0x3F)
   }
 
-  def exists(a: Int): Boolean = getValue(a) == 1
+  def exists(a: Long): Boolean = getValue(a) == 1L
 
-  def getValue(e: Int): Int = BM(e >> 5) >> (e & 0x1F) & 1
+  def getValue(e: Long): Long = BM((e >> 6).toInt) >> (e & 0x3F) & 1
 
   def show(num: Int): Unit = {
     (0 until num).foreach {
       i =>
-        val tmpVal: Array[Int] = new Array[Int](32)
-        var temp: Int = BM(i)
+        val tmpVal: Array[Long] = new Array[Long](64)
+        var temp: Long = BM(i)
         tmpVal.indices.foreach {
           j =>
             tmpVal(j) |= (temp & 1)
@@ -35,9 +37,10 @@ class BitMap(size: Int) extends Cloneable with Serializable {
 
 object BitMap {
   def main(args: Array[String]): Unit = {
-    val bMap: BitMap = new BitMap(320)
-    Array(1, 2, 3, 5, 8, 30, 32, 64, 56, 159, 120, 21, 17, 35, 45, 320).foreach(bMap.insert)
-    Array(2, 3, 5, 8).foreach(bMap.insert)
+    val bMap: BitMap = new BitMap(320L)
+    Array(1, 2, 3, 5, 8, 30, 32, 64, 56, 159, 120, 21, 17, 35, 45, 320)
+      .foreach(x => bMap.insert(x.toLong))
+    Array(2, 3, 5, 8).foreach(x => bMap.insert(x.toLong))
 
     println(s">>> Key: 159, Value: ${bMap.getValue(159)}.")
     if (bMap.exists(320)) {
