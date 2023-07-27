149c4b40b69ed7641a8bfdda6e460bfe3389c7eb
Wallace Huang <h417652303@gmail.com>
2023-07-27 15:37:27 +0800

    [update] add new mock events

--- demo/FlinkDemo/src/main/scala/com/notalk/flink/demo/FlinkCEPDemo.scala
+++ demo/FlinkDemo/src/main/scala/com/notalk/flink/demo/FlinkCEPDemo.scala
@@ -2,11 +2,13 @@ package com.notalk.flink.demo
 
 import com.notalk.flink.demo.common.LogSupport
 import com.notalk.flink.demo.event.LoginEvent
+import org.apache.flink.cep.nfa.aftermatch.AfterMatchSkipStrategy
 import org.apache.flink.cep.scala.pattern.Pattern
 import org.apache.flink.cep.scala.{CEP, PatternStream}
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala.{KeyedStream, StreamExecutionEnvironment, createTypeInformation}
 import org.apache.flink.streaming.api.windowing.time.Time
+
 /**
  * Author: biyu.huang
  * Date: 2023/7/27 14:38
@@ -31,13 +33,17 @@ object FlinkCEPDemo extends LogSupport {
         LoginEvent("user_3", "192.168.19.4", "fail", 30000L),
         LoginEvent("user_3", "192.168.19.5", "success", 35000L),
         LoginEvent("user_4", "192.168.19.15", "success", 50000L),
+        LoginEvent("user_5", "192.168.21.112", "fail", 51000L),
+        LoginEvent("user_5", "192.168.23.13", "fail", 52000L),
+        LoginEvent("user_5", "192.168.34.12", "fail", 53000L),
+        LoginEvent("user_5", "192.168.44.11", "fail", 54000L),
       )
       .assignAscendingTimestamps(_.eventTime)
       .keyBy(r => r.userId)
 
     stream.print("login_event ")
 
-    val pattern: Pattern[LoginEvent, LoginEvent] = Pattern
+    val threeTimesFailPattern: Pattern[LoginEvent, LoginEvent] = Pattern
       .begin[LoginEvent]("first")
       .where(r => r.eventType.equals("fail"))
       .next("second")
@@ -45,10 +51,8 @@ object FlinkCEPDemo extends LogSupport {
       .next("third")
       .where(r => r.eventType.equals("fail"))
       .within(Time.seconds(5))
-
-    val patternedStream: PatternStream[LoginEvent] = CEP.pattern(stream, pattern)
-
-    patternedStream
+    val failedStream: PatternStream[LoginEvent] = CEP.pattern(stream, threeTimesFailPattern)
+    failedStream
       .select((pattern: scala.collection.Map[String, Iterable[LoginEvent]]) => {
         val first = pattern("first").iterator.next()
         val second = pattern("second").iterator.next()
@@ -56,7 +60,27 @@ object FlinkCEPDemo extends LogSupport {
 
         (first.userId, first.ip, second.ip, third.ip)
       })
-      .printToErr("result ")
+      .printToErr("fail_result ")
+
+    val successPattern: Pattern[LoginEvent, LoginEvent] = Pattern
+      .begin[LoginEvent]("fail", AfterMatchSkipStrategy.skipPastLastEvent()) // skipToFirst("success")
+      .optional
+      .where(r => r.eventType.equals("fail"))
+      .followedBy("success")
+      .where(r => r.eventType.equals("success"))
+      .within(Time.seconds(30))
+    val successStream = CEP.pattern(stream, successPattern)
+    successStream.select((pattern: scala.collection.Map[String, Iterable[LoginEvent]]) => {
+      val fail = if (pattern.contains("fail")) {
+        pattern("fail").iterator.next()
+      } else {
+        LoginEvent("", "", "", 0L)
+      }
+      val success = pattern("success").iterator.next()
+
+      (success.userId, fail.ip, success.ip)
+    })
+      .printToErr("success_result")
 
     scalaEnv.execute()
     logger.info("stop to run Flink CEP demo")
